0b7f942b1ce994ceeee78b44a89e93054da06da6
samuel-williams-ed <70590831+samuel-williams-ed@users.noreply.github.com>
2022-05-21 13:18:31 +0100

    Add files via upload

--- DayActivity.java
+++ DayActivity.java
@@ -15,12 +15,13 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DayActivity extends AppCompatActivity {
+public class DayActivity extends AppCompatActivity implements editEventFragment.FragmentCloser {
 
     public static final String TAG = "DayActivity";
     TextView dateText;
@@ -30,8 +31,10 @@ public class DayActivity extends AppCompatActivity {
     Button saveEventsText;
     Button homeButton;
 
-    ImageView backgroundImg;
     String userValues;
+    String date;
+
+    ImageView backgroundImg;
     ListView eventDataDisplay;
     DatabaseHelper dh;
     EventsDatabaseHelper edh;
@@ -43,6 +46,8 @@ public class DayActivity extends AppCompatActivity {
 //    EditText testText;
 //    Button saveTestText;
     List<EventObject> events_list;
+    Fragment eventFrag;
+
 
 // TODO update events display to a scroll to fit multiple events
 
@@ -57,7 +62,6 @@ public class DayActivity extends AppCompatActivity {
         dateText = findViewById(R.id.dayLayout_dateText_TV);
         reminderText = findViewById(R.id.dayView_reminder1_TV);
         eventsText = findViewById(R.id.dayView_event1_TV);
-
         saveEventsText = findViewById(R.id.dayView_addEvent1_Bu);
         homeButton = findViewById(R.id.DayView_button_returnHome);
         backgroundImg = findViewById(R.id.dayView_backImage_IV);
@@ -66,9 +70,9 @@ public class DayActivity extends AppCompatActivity {
         //retrieve data from incoming intents
         Intent catchIncomingIntent = getIntent();
         String raw_date = catchIncomingIntent.getStringExtra("date");
-        String date = clean(raw_date);
+        date = clean(raw_date);
 
-        //initialise database
+        //initialise databases
         dh = new DatabaseHelper(DayActivity.this, "dates_database");
         edh = new EventsDatabaseHelper(DayActivity.this);
         try {
@@ -81,7 +85,6 @@ public class DayActivity extends AppCompatActivity {
         //set page data
         setPageData(date);
 
-
         //return home click listener
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,12 +94,10 @@ public class DayActivity extends AppCompatActivity {
             }
         });
 
-
-        // save Text to database
+        // save User Text to database
         saveEventsText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 try{
                     userValues = eventsText.getText().toString();
                 } catch (Exception e) {
@@ -104,14 +105,11 @@ public class DayActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
                 Log.d(TAG, "Test button has been pressed");
-
                 newEvent = new EventObject(date, userValues);
-
                 boolean itsBeenAdded = edh.addEvent(newEvent);
                 Log.d(TAG, "Database Insert = " + itsBeenAdded);
 
                 //update UI
-                //TODO check displayEvents() is drawing from edh
                 displayEvents(today_DO);
                 eventsText.setText("");
             }
@@ -122,23 +120,22 @@ public class DayActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
                 //fragment pop-up
                 String name = (String) parent.getItemAtPosition(position);
+                EventObject databaseEvent = edh.getEventByNameAndDate(name, calcId(date));
+                String databaseId = edh.getDatabaseIdFromEvent(databaseEvent);
                 FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                editEventFragment frag_editEvent = editEventFragment.newInstance(date, name);
+                editEventFragment frag_editEvent = editEventFragment.newInstance(date, name, databaseId);
                 ft.replace(R.id.dayLayout_placeholder, frag_editEvent);
                 ft.commit();
+                Log.d(TAG, "eventDataDisplaylistener() complete");
                 //set other view elements as hidden
                 dateText.setVisibility(View.GONE);
                 reminderText.setVisibility(View.GONE);
                 eventsText.setVisibility(View.GONE);
-
-
             }
         });
     }
 
-
     // ####### DayActivity Methods ########
-
     private String clean(String raw_date) {
         if(raw_date.length() == 9) {
             return "0" + raw_date;
@@ -147,52 +144,56 @@ public class DayActivity extends AppCompatActivity {
     }
 
     private void setPageData(String date){
-        //set page data
+        Log.d(TAG, "Setting page data; ");
         dateText.setText(date);
         setBackgroundImage(backgroundImg, date);
         displayEvents(today_DO);
-
-//        //testing
-//        eventsText = findViewById(R.id.dayView_event1_TV);
-//        saveEventsText = findViewById(R.id.dayView_addEvent1_Bu);
     }
 
     //TODO currently - dh.getEvents() is returning a list of Strings from database; only the column 'DATA' is being read from database by getEvents()
     //TODO change this to returning an Event object - so when selected by user we can open an EventView Activity, where edit etc.,
+
+    // set the Adapter for eventDataDisplay ListView which displays the name of each event to the user in DayActivity
     private void displayEvents(DateObject eventDate){
-
         events_list = edh.getEventObjects(eventDate);
-
         List<String> events_data_list = new ArrayList<>();
-
         for(EventObject event : events_list){
             events_data_list.add(event.getName());
         }
-
         eventsArrayAdapter = new ArrayAdapter(DayActivity.this, android.R.layout.simple_list_item_1, events_data_list);
         eventDataDisplay.setAdapter(eventsArrayAdapter);
     }
 
-
     private void setBackgroundImage(ImageView imageHolder, String date){
-
         int imageID = getImageID(date);
-
         Drawable image = getDrawable(R.drawable.corgi1);
         //^ testing v
         //Drawable image = getDrawable(imageID);
         imageHolder.setImageDrawable(image);
-
     }
 
     //TODO build getImageID()
     private int getImageID(String date) {
-        
-        
         return 1;
     }
 
 
+    @Override
+    public void removeFragment() {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        eventFrag = (Fragment) getSupportFragmentManager().findFragmentById(R.id.dayLayout_placeholder);
+        ft.remove(eventFrag);
+        ft.commit();
+        setPageData(date);
+    }
+
+    public String calcId(String pDate){
+        Log.d(TAG, "calcId: parameter date = " + pDate);
+        String editedDate = pDate.substring(6, 10) + pDate.substring(3, 5) + pDate.substring(0, 2);
+
+        Log.d(TAG, "calcId: edited date = " + editedDate);
+        return editedDate;
+    }
 }
 
 
--- editEventFragment.java
+++ editEventFragment.java
@@ -1,5 +1,6 @@
 package com.example.calendartutorial;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -25,30 +27,61 @@ public class editEventFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "01/11/1993";
     private static final String ARG_PARAM2 = "Event failed to load";
+    private static final String ARG_PARAM3 = "0";
     private static final String TAG = "editEventFragment.java";
 
     private String mParamDate;
     private String mParamName;
+    private String mParamDatabaseId;
 
     TextView nameBox;
     TextView dateBox;
     TextView locationBox;
     TextView detailsBox;
     FrameLayout frame;
+    FragmentCloser editEventFrag;
+
+    Button add_name_button;
+    Button add_location_button;
+    Button add_details_button;
+    Button delete_event_button;
+
+    String userValues_name;
+    String userValues_location;
+    String userValues_details;
+
+    EventsDatabaseHelper edh;
+    EventObject event;
+
+    public interface FragmentCloser {
+        public void removeFragment();
+    }
 
 
     public editEventFragment() {
         // Required empty public constructor
     }
 
-    public static editEventFragment newInstance(String date, String param_name) {
+    public static editEventFragment newInstance(String date, String param_name, String database_id) {
         editEventFragment fragment = new editEventFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, date);
         args.putString(ARG_PARAM2, param_name);
+        args.putString(ARG_PARAM3, database_id);
 
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+        if(context instanceof FragmentCloser){
+            editEventFrag = (FragmentCloser) context;
+        } else {
+            Log.d(TAG, "The interface was not implemented in the parent Activtiy!");
+        }
+        edh = new EventsDatabaseHelper(context);
     }
 
     @Override
@@ -57,6 +90,11 @@ public class editEventFragment extends Fragment {
         if (getArguments() != null) {
             mParamDate = getArguments().getString(ARG_PARAM1);
             mParamName = getArguments().getString(ARG_PARAM2);
+            mParamDatabaseId = getArguments().getString(ARG_PARAM3);
+
+            Log.d(TAG, "initialising event Object in editEventFragment.java, mParamDatabaseId = " + mParamDatabaseId);
+            event = edh.getEventByDatabaseId(mParamDatabaseId); //TODO find why this failed
+            Log.d(TAG, "We have an event!");
         }
     }
 
@@ -71,25 +109,83 @@ public class editEventFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
 
+        frame = view.findViewById(R.id.fragEditEvent_frame_FL);
+
         dateBox= view.findViewById(R.id.EditEventFrag_date_TV);
         nameBox = view.findViewById(R.id.EditEventFrag_name_TV);
         locationBox = view.findViewById(R.id.EditEventFrag_location_TV);
         detailsBox = view.findViewById(R.id.EditEventFrag_details_TV);
-        frame = view.findViewById(R.id.fragEditEvent_frame_FL);
+
+        add_name_button = view.findViewById(R.id.frag_editName_addButton_BU);
+        add_location_button = view.findViewById(R.id.frag_editLocation_addButton_BU);
+        add_details_button = view.findViewById(R.id.frag_editDetails_addButton_BU);
+        delete_event_button = view.findViewById((R.id.EditEventFrag_Delete_BU));
+
 
         Log.d(TAG, "dateBox text = " + mParamDate);
 
         dateBox.setText(mParamDate);
         nameBox.setText(mParamName);
 
+        //edit the name
+        add_name_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    userValues_name = nameBox.getText().toString();
+                } catch (Exception e) {
+                    Log.d(TAG, "AddNameButton: error getting user values from testText object");
+                    e.printStackTrace();
+                }
+                Log.d(TAG, "AddNameButton: button has been pressed");
+                edh.editEventName(event, userValues_name);
+
+                Log.d(TAG, "AddNameButton: Database changed! Name " + userValues_name + " added to database.");
+            }
+        });
+
+        delete_event_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "edh.deleteEvent() firing");
+                edh.deleteEvent(event);
+                Log.d(TAG, "editEventFrag.removeFragment() firing");
+                editEventFrag.removeFragment();
+            }
+        });
+
+        // on frame click close fragment
         frame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 //                frame.setBackgroundColor(getResources().getColor(R.color.teal_200, null));
-
+                editEventFrag.removeFragment();
             }
         });
 
     }
 
 }
+
+//TODO close the fragment logic
+// create a functional interface with fragment operations
+// listener event should fire on DayActivity via an interface method:
+// define an interface type in the fragment
+// make the activity implement the interface:
+// - define a listener of the interface type in the Activity
+// - - this will be the Activity's instance of/reference to the fragment
+// - define interface
+// - - declare events/methods to fire
+// - @Override onAttach() method to initialise the listener as the 'context' passed as a param to onAttach().
+// - - N.B. may need to cast this to the interface type.
+// - - N.B. this checks the listener (context from the onAttach() method) does implement the interface.
+// - define onCLickListener()
+// // - - fire events/methods declared earlier as part of interface
+// in Activity, implement interface
+// in onCreate() initialise fragment, getSupportFragmentManager().findFragmentById(R.id.frag_name);
+// @Override the interface's method being fired (in Activity, onClickListener) logic
+// - so...
+// begin transaction with fragManager
+// identify this editEventFragment object instance - findFragmentById()  ***
+// call remove() passing the fragment instance
+// commit transaction
